ee012c9dde582e677f86716ced97e97866045c9a
Hugo Alexander <165641301+Galexhu@users.noreply.github.com>
2025-10-30 14:44:30 +0000

    ya parece que va el leranpack

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+const fromEuroToDollar = function(valueInEuro){
+    let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+function fromDollarToYen =
+
+    let valueInDollar = valueInEuro+1,07;
+
+    return valueInDollar;
+
+}
+module.exports={sum,fromEuroToDollar}
