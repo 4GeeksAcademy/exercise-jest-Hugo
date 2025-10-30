91279e4620fed3ff0195babf319d43b199dcdb96
Hugo Alexander <165641301+Galexhu@users.noreply.github.com>
2025-10-30 17:10:30 +0000

    ahora si que si

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let oneEuroIs = {
-    "JPY": 156.5, // japan yen
-    "USD": 1.07, // us dollar
-    "GBP": 0.87, // british pound
+    "JPY": 156.5, 
+    "USD": 1.07, 
+    "GBP": 0.87, 
 }
 
  const fromDollarToYen = function(valueInDollar){
