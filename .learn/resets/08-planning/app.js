d034ab10044c15a024caa394e1b986a4f1dff6cb
Hugo Alexander <165641301+Galexhu@users.noreply.github.com>
2025-10-30 17:08:18 +0000

    por fingit add .

---
+++ .learn/resets/08-planning/app.js
@@ -0,0 +1,17 @@
+
+
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+const fromEuroToDollar = function(valueInEuro){
+function fromDollarToYen =
+
+    let valueInDollar = valueInEuro+1,07;
+
+    return valueInDollar;
+
+}
+module.exports={sum,fromEuroToDollar}
